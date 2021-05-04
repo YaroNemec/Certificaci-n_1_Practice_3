2c265e79d98835fbd836812781ec51468159b828
YaroNemec <62666356+YaroNemec@users.noreply.github.com>
2021-05-04 11:59:07 -0400

    P3-002: Agregé los metodos Delete and Update en la clase StudentController

--- Practice_3/Practice_3/Controllers/StudentsController.cs
+++ Practice_3/Practice_3/Controllers/StudentsController.cs
@@ -9,31 +9,31 @@ namespace Practice_3.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class StudentsController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<StudentsController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public StudentsController(ILogger<StudentsController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<Student> Get()
+        [HttpPut]
+        public Student UpdateStudent([FromBody] Student estudiante)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new Student
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            estudiante.NameStudent = "updated";
+            return estudiante;
+        }
+
+        [HttpDelete]
+        public Student DeleteStudent([FromBody] Student estudiante)
+        {
+            return estudiante;
         }
     }
 }
