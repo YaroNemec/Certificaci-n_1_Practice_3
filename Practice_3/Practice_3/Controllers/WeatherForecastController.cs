7f534c0f238c02c97f559ec1380a7525f2836e3c
YaroNemec <62666356+YaroNemec@users.noreply.github.com>
2021-05-04 11:43:09 -0400

    P3-001: Modifiqué el archivo WhatherForecast.cs y ahora es Studen

--- Practice_3/Practice_3/Controllers/WeatherForecastController.cs
+++ Practice_3/Practice_3/Controllers/WeatherForecastController.cs
@@ -24,10 +24,10 @@ namespace Practice_3.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Student> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new Student
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
