29bd8e49e6d5419ce7e0630e344e88bcf64308c9
YaroNemec <62666356+YaroNemec@users.noreply.github.com>
2021-05-04 12:03:46 -0400

    P3-002: Agregéel metodo GetStudent y eliminé los datos innecesarios

--- Practice_3/Practice_3/Controllers/StudentsController.cs
+++ Practice_3/Practice_3/Controllers/StudentsController.cs
@@ -11,18 +11,18 @@ namespace Practice_3.Controllers
     [Route("[controller]")]
     public class StudentsController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<StudentsController> _logger;
 
         public StudentsController(ILogger<StudentsController> logger)
         {
-            _logger = logger;
         }
 
+
+        [HttpGet]
+        public List<Student> GetStudent()
+        {
+            return new List<Student>();
+            
+        }
         [HttpPost]
         public Student CreateStudent([FromBody] string studentName)
         {
