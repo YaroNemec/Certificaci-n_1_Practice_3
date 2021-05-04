b2bddceffd577f68c63fff8e313aa4ceda8ca02f
YaroNemec <62666356+YaroNemec@users.noreply.github.com>
2021-05-04 11:44:24 -0400

    P3-001: Agregé el atributo NameStudent a la Clase Student

--- Practice_3/Practice_3/Student.cs
+++ Practice_3/Practice_3/Student.cs
@@ -2,14 +2,10 @@ using System;
 
 namespace Practice_3
 {
-    public class WeatherForecast
+    public class Student
     {
-        public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public String NameStudent { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
     }
 }
